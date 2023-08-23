0422bea4ee727ac40754a7d520dbe3037a3510e6
OrFrenkelf1 <or.frenkelf1@gmail.com>
2023-08-23 22:59:46 +0300

    new

--- Mobile Git/Assets/Scripts/RandomMove.cs
+++ Mobile Git/Assets/Scripts/RandomMove.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class RandomMove : MonoBehaviour
 {
-    public Vector3 targetPosition = new Vector3(0.26f, 0.35f, 0);
-    float speed = 0.4f;
+    public Vector3 targetPosition;
+    public Vector3 targetRotation;
+
+    private float speed = 0.4f;
+
     void Update()
     {
         transform.localPosition = Vector3.Lerp(transform.localPosition, targetPosition, Time.deltaTime * speed);
+        transform.Rotate(targetRotation * speed * Time.deltaTime);
     }
 }
